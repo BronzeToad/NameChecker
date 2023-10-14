887d96d65a1a79f448efc6de645a33b73907b7eb
BronzeToad <github@ajpf.me>
2023-10-13 23:58:07 -0500

    Update domain_checker.py

--- src/domain_checker.py
+++ src/domain_checker.py
@@ -8,14 +8,14 @@ from cfg.config_helper import ConfigHelper
 
 # =============================================================================================== #
 
-class DomainChecker:
 
+class DomainChecker:
     def __init__(
         self,
         host_names: List[str],
         config_helper: ConfigHelper,
         max_retries: Optional[int] = None,
-        endings: Optional[List[str]] = None
+        endings: Optional[List[str]] = None,
     ) -> None:
         self.host_names = host_names
         self.cfg = config_helper
@@ -23,20 +23,19 @@ class DomainChecker:
         self.endings = endings
         self.__post_init__()
 
-
     def __post_init__(self) -> None:
         """Post initialization to set class properties."""
         self.max_retries = self.get_max_retries()
-        self.endings = self.endings or ['com']
+        self.endings = self.endings or ["com"]
         self.domains = self.get_domains()
         self.api_headers = self.get_api_headers()
-
 
     def get_max_retries(self) -> int:
         """Get the maximum number of retries allowed."""
         if self.max_retries is None or self.max_retries <= 0:
             return self.cfg.godaddy_max_retries
-
+        else:
+            return self.max_retries
 
     def get_api_headers(self) -> Dict[str, str]:
         """Generate the API headers required for the GoDaddy API request."""
@@ -44,32 +43,30 @@ class DomainChecker:
         api_secret = self.cfg.godaddy_api_secret
 
         return {
-            'Authorization': f"sso-key {api_key}:{api_secret}",
-            'Accept': 'application/json'
+            "Authorization": f"sso-key {api_key}:{api_secret}",
+            "Accept": "application/json",
         }
-
 
     def get_domains(self) -> List[str]:
         """Generate the list of domains to check."""
         domains = []
         for hostname in self.host_names:
-            for ending in self.endings:
+            for ending in self.endings or []:
                 domains.append(f"{hostname}.{ending}")
         return domains
-
 
     def check_domain(self, domain: str) -> bool:
         """Check the availability of a single domain."""
         endpoint = f"{self.cfg.godaddy_api_url}?domain={domain}"
 
-        for _ in range(self.max_retries):
+        for _ in range(self.max_retries or 0):
             response = requests.get(endpoint, headers=self.api_headers)
 
             if response.status_code == 200:
                 data = response.json()
-                return data['available']
+                return data["available"]
 
-            if 'TOO_MANY_REQUESTS' in response.text:
+            if "TOO_MANY_REQUESTS" in response.text:
                 print("ERROR: TOO_MANY_REQUESTS -> pausing for 30 seconds...")
                 time.sleep(30)
             else:
@@ -79,14 +76,13 @@ class DomainChecker:
         print(f"ERROR: Failed to fetch data after {self.max_retries} retries.")
         return False
 
-
     def check(self) -> List[Dict[str, bool]]:
         """Check the availability of all domains and return the results."""
         results = []
 
         for domain in self.domains:
             is_available = self.check_domain(domain)
-            host_name, domain_ending = domain.rsplit('.', 1)
-            results.append({'name': host_name, domain: is_available})
+            host_name, domain_ending = domain.rsplit(".", 1)
+            results.append({"name": host_name, domain: is_available})
 
         return results
