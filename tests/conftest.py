ade923997ab1b39430b252335e2c5f10b18d3c3b
BronzeToad <github@ajpf.me>
2023-10-15 00:03:05 -0500

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,8 @@
 import pytest
+
+import src.utils.toad_utils as ToadUtils
 from src.utils.config_helper import ConfigHelper, EnvType
+
 
 # =============================================================================================== #
 
@@ -11,3 +14,12 @@ def env_type(request):
 @pytest.fixture
 def config_helper(env_type):
     return ConfigHelper(env_type, test_mode=True)
+
+@pytest.fixture
+def toad_utils():
+    return ToadUtils
+
+@pytest.fixture
+def project_root(toad_utils):
+    return toad_utils.find_project_root('NameChecker', test_mode=True)
+
