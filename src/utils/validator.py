57468b277b2c29dbaef6406c295506b817101f26
BronzeToad <github@ajpf.me>
2023-10-15 08:43:57 -0500

    Update validator.py

--- src/utils/validator.py
+++ src/utils/validator.py
@@ -32,19 +32,21 @@ class Validator:
             raise ValueError(f"Invalid validator type: {self.validator_type}")
 
 
-    def _check_integer(self, error_msg_base: str, value: int) -> None:
-        if not value:
+    def _check_integer(self, error_msg_base: str, value: int) -> bool:
+        if value is None:
             raise self.error_type(f"{error_msg_base} cannot be empty.")
         if not isinstance(value, int):
             raise self.error_type(f"{error_msg_base} must be an integer.")
+        return True
 
 
-    def _check_string(self, error_msg_base: str, value: str) -> None:
+    def _check_string(self, error_msg_base: str, value: str) -> bool:
         """Validates that the provided value is a string."""
-        if not value:
+        if value is None:
             raise self.error_type(f"{error_msg_base} cannot be empty.")
         if not isinstance(value, str):
             raise self.error_type(f"{error_msg_base} must be a string.")
+        return True
 
 
     def integer(
@@ -52,14 +54,15 @@ class Validator:
         value: int,
         min_value: Optional[int] = None,
         max_value: Optional[int] = None
-    ) -> None:
+    ) -> bool:
         """Validates that the provided value is an integer."""
         error_msg_base = f"Value {value} is invalid: number value"
-        self._check_integer(error_msg_base, value)
-        if min_value is not None and value < min_value:
-            raise self.error_type(f"{error_msg_base} must be {min_value} or greater.")
-        if max_value is not None and value > max_value:
-            raise self.error_type(f"{error_msg_base} must be {max_value} or less.")
+        if self._check_integer(error_msg_base, value):
+            if min_value is not None and value < min_value:
+                raise self.error_type(f"{error_msg_base} must be {min_value} or greater.")
+            if max_value is not None and value > max_value:
+                raise self.error_type(f"{error_msg_base} must be {max_value} or less.")
+        return True
 
 
     def directory(
@@ -68,16 +71,17 @@ class Validator:
         check_absolute_path: Optional[bool] = True,
         check_exists: Optional[bool] = True,
         check_is_dir: Optional[bool] = True,
-    ) -> None:
+    ) -> bool:
         """Validates a directory path."""
         error_msg_base = f"Directory {dir_path} is invalid: directory"
-        self._check_string(error_msg_base, dir_path)
-        if check_absolute_path and not os.path.isabs(dir_path):
-            raise self.error_type(f"{error_msg_base} must be an absolute path.")
-        if check_exists and not os.path.exists(dir_path):
-            raise self.error_type(f"{error_msg_base} does not exist.")
-        if check_is_dir and not os.path.isdir(dir_path):
-            raise self.error_type(f"{error_msg_base} is not a directory.")
+        if self._check_string(error_msg_base, dir_path):
+            if check_absolute_path and not os.path.isabs(dir_path):
+                raise self.error_type(f"{error_msg_base} must be an absolute path.")
+            if check_exists and not os.path.exists(dir_path):
+                raise self.error_type(f"{error_msg_base} does not exist.")
+            if check_is_dir and not os.path.isdir(dir_path):
+                raise self.error_type(f"{error_msg_base} is not a directory.")
+        return True
 
 
     def filename(
@@ -85,16 +89,17 @@ class Validator:
         filename: str,
         invalid_chars=None,
         check_path_seq: Optional[bool] = True,
-    ) -> None:
+    ) -> bool:
         """Validates a filename."""
         if invalid_chars is None:
             invalid_chars = ['<', '>', ':', '"', '/', '\\', '|', '?', '*']
         error_msg_base = f"Filename {filename} is invalid: filename"
-        self._check_string(error_msg_base, filename)
-        if invalid_chars is not None and any(char in filename for char in invalid_chars):
-            raise self.error_type(f"{error_msg_base} contains one or more invalid characters.")
-        if check_path_seq and ".." in filename:
-            raise self.error_type(f"{error_msg_base} contains a potentially unsafe path sequence.")
+        if self._check_string(error_msg_base, filename):
+            if invalid_chars is not None and any(char in filename for char in invalid_chars):
+                raise self.error_type(f"{error_msg_base} contains one or more invalid characters.")
+            if check_path_seq and ".." in filename:
+                raise self.error_type(f"{error_msg_base} contains a potentially unsafe path sequence.")
+        return True
 
 
     def url(
@@ -102,15 +107,16 @@ class Validator:
         url: str,
         check_schema: Optional[bool] = True,
         check_domain: Optional[bool] = True,
-    ) -> None:
+    ) -> bool:
         error_msg_base = f"URL {url} is invalid: URL"
-        self._check_string(error_msg_base, url)
-        parsed_url = urlparse(url)
-        if check_schema:
-            if not parsed_url.scheme or parsed_url.scheme not in ['http', 'https']:
-                raise self.error_type(f"{error_msg_base} must have a valid scheme.")
-        if check_domain and not parsed_url.netloc:
-            raise self.error_type(f"{error_msg_base} must have a valid domain.")
+        if self._check_string(error_msg_base, url):
+            parsed_url = urlparse(url)
+            if check_schema:
+                if not parsed_url.scheme or parsed_url.scheme not in ['http', 'https']:
+                    raise self.error_type(f"{error_msg_base} must have a valid scheme.")
+            if check_domain and not parsed_url.netloc:
+                raise self.error_type(f"{error_msg_base} must have a valid domain.")
+        return True
 
 
     def api_token(
@@ -120,14 +126,15 @@ class Validator:
         expected_prefix: Optional[str] = None,
         expected_suffix: Optional[str] = None,
         invalid_chars: Optional[List[str]] = None
-    ) -> None:
+    ) -> bool:
         error_msg_base = f"API token {token_value} is invalid: API token"
-        self._check_string(error_msg_base, token_value)
-        if expected_length is not None and len(token_value) != expected_length:
-            raise self.error_type(f"{error_msg_base} must be {expected_length} characters.")
-        if expected_prefix is not None and not token_value.startswith(expected_prefix):
-            raise self.error_type(f"{error_msg_base} must start with {expected_prefix}.")
-        if expected_suffix is not None and not token_value.endswith(expected_suffix):
-            raise self.error_type(f"{error_msg_base} must end with {expected_suffix}.")
-        if invalid_chars is not None and any(char in token_value for char in invalid_chars):
-            raise self.error_type(f"{error_msg_base} contains one or more invalid characters.")
+        if self._check_string(error_msg_base, token_value):
+            if expected_length is not None and len(token_value) != expected_length:
+                raise self.error_type(f"{error_msg_base} must be {expected_length} characters.")
+            if expected_prefix is not None and not token_value.startswith(expected_prefix):
+                raise self.error_type(f"{error_msg_base} must start with {expected_prefix}.")
+            if expected_suffix is not None and not token_value.endswith(expected_suffix):
+                raise self.error_type(f"{error_msg_base} must end with {expected_suffix}.")
+            if invalid_chars is not None and any(char in token_value for char in invalid_chars):
+                raise self.error_type(f"{error_msg_base} contains one or more invalid characters.")
+        return True
